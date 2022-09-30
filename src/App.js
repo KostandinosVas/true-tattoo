490028f59bafa61809e09f5ed73bd38f8de07741
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-09-30 21:13:50 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+// import animate on scroll
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 // import components
 import Hero from './components/Hero';
 
 const App = () => {
+  // aos initialization
+  Aos.init({
+    duration: 1800,
+    offset: 100,
+  });
   return (
     <div className='max-w-[1920px] mx-auto overflow-hidden'>
       <Hero />
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,11 +4,27 @@ import { heroData } from '../data';
 
 const Hero = () => {
   // destructure hero data
-  const { title } = heroData;
+  const { title, subtitle, textBtn } = heroData;
   return (
-    <section className='bg-hero bg-cover bg-center min-h-[40vh] lg:h-[948px] bg-no-repeat text-white relative'>
-      <div className='container mx-auto'>
-        <h1>{title}</h1>
+    <section className='bg-hero bg-cover bg-center min-h-[40vh] lg:h-[948px] bg-no-repeat relative'>
+      <div className='container mx-auto min-h-[40vh] lg:h-full flex items-center lg:justify-end'>
+        {/* text & btn */}
+        <div className='text-white text-center lg:text-left lg:max-w-[640px]'>
+          <h1 className='h1' data-aos='fade-up' data-aos-delay='400'>
+            {title}
+          </h1>
+          <p className='mb-10 lg:mb-20' data-aos='fade-up' data-aos-delay='600'>
+            {subtitle}
+          </p>
+          {/* btn */}
+          <button
+            className='bg-slate-50'
+            data-aos='fade-up'
+            data-aos-delay='800'
+          >
+            {textBtn}
+          </button>
+        </div>
         {/* outline text */}
         <div className='hidden xl:flex absolute -bottom-2 right-0 left-0 before:content-outlineText'></div>
       </div>
