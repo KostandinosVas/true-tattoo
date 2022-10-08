bd4a94141cd089dff1072a6748bc086f9fdb040b
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-10-08 10:53:30 +0300

    added

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,14 +1,28 @@
 import React from 'react';
+// import motion
+import { motion } from 'framer-motion';
+// import variants
+import { fadeIn } from '../variants';
 
 const Copyright = () => {
   return (
     <div className='bg-dark py-6 border-t border-[#2B2B2B] text-[#DBDBDB]'>
       <div className='container mx-auto'>
-        <div className='tracking-[0.02em] text-base'>
+        <motion.div
+          initial={{ y: 40, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{
+            type: 'tween',
+            duration: 0.8,
+            delay: 0.2,
+          }}
+          viewport={{ once: false, amount: 0 }}
+          className='tracking-[0.02em] text-base'
+        >
           &copy; 2022
           <span className='font-semibold text-white'>The Void Tattoo.</span>
           All rights reserved.
-        </div>
+        </motion.div>
       </div>
     </div>
   );
